f4d0368c7d1c0e13060c6def053b37ec8871a975
Guilherme dos Santos <guilherme.devsoft@gmail.com>
2026-09-03 23:14:08 -0300

    add application-test, editando classe test e application yml

--- src/test/java/com/pedacinhodemaria/PedacinhoBackendApplicationTests.java
+++ src/test/java/com/pedacinhodemaria/PedacinhoBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.pedacinhodemaria;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PedacinhoBackendApplicationTests {
 
 	@Test
